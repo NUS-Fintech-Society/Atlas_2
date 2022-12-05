cb3f038a5dd65f2952283b2c6e24db2d8c223b17
woowenjun99 <woowenjun99@gmail.com>
2022-12-06 00:55:05 +0800

    Update the name and remove the image

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -60,13 +60,6 @@ export default function Example() {
                   <div>
                     <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open user menu</span>
-                      <Image
-                        height={8}
-                        width={8}
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
                     </Menu.Button>
                   </div>
                   <Transition
--- src/server/trpc/router/member/index.ts
+++ src/server/trpc/router/member/index.ts
@@ -1,5 +1,5 @@
 import { router } from '~/server/trpc/trpc'
-import { getMemberProfile, getMemberImage } from './get'
+import { getMemberProfile, getMemberImage } from './read'
 import { updateMemberImage } from './update'
 import { deleteMemberImage } from './delete'
 
