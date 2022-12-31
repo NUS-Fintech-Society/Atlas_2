edfbc63e40a0da792f6f53121c9d2d965a6f447b
woowenjun99 <woowenjun99@gmail.com>
2022-12-31 17:09:29 +0800

    Add in the redirect

--- next.config.mjs
+++ next.config.mjs
@@ -9,15 +9,24 @@ const withBundleAnalyzer = bundleAnalyzer({
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'))
 
 /** @type {import("next").NextConfig} */
 const config = {
+  redirects: () => {
+    return [
+      {
+        source: '/',
+        destination: '/auth/login',
+        permanent: false,
+      },
+    ]
+  },
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
-};
-export default withBundleAnalyzer(config);
+}
+export default withBundleAnalyzer(config)
