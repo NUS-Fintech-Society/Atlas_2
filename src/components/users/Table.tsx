b6bd7978a10d57b245d09aa2a1126cf71ade58a4
woowenjun99 <woowenjun99@gmail.com>
2023-01-18 23:16:13 +0800

    Refactor the data table

--- src/components/users/Table.tsx
+++ src/components/users/Table.tsx
@@ -1,6 +1,17 @@
-import { type Table as ReactTable, flexRender } from '@tanstack/react-table'
+import LoadingScreen from '~/components/common/LoadingScreen'
+import { trpc } from '~/utils/trpc'
+import {
+  type Table as ReactTable,
+  useReactTable,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  type ColumnDef,
+  flexRender,
+} from '@tanstack/react-table'
+import { useMemo, useState } from 'react'
 
-export type User = {
+type User = {
   name: string | null
   roles: string | null
   department: string | null
@@ -8,7 +19,7 @@ export type User = {
   id: string
 }
 
-export const TableHeader = ({ table }: { table: ReactTable<User> }) => {
+const TableHeader = ({ table }: { table: ReactTable<User> }) => {
   return (
     <thead>
       {table.getHeaderGroups().map((headerGroup) => (
@@ -31,7 +42,7 @@ export const TableHeader = ({ table }: { table: ReactTable<User> }) => {
   )
 }
 
-export const TableBody = ({ table }: { table: ReactTable<User> }) => {
+const TableBody = ({ table }: { table: ReactTable<User> }) => {
   return (
     <tbody className="text-center">
       {table.getRowModel().rows.map((row) => {
@@ -49,4 +60,86 @@ export const TableBody = ({ table }: { table: ReactTable<User> }) => {
       })}
     </tbody>
   )
+}
+
+const Table = ({
+  data,
+  columns,
+}: {
+  data: User[]
+  columns: ColumnDef<User>[]
+}) => {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  })
+
+  return (
+    <table>
+      <TableHeader table={table} />
+      <TableBody table={table} />
+    </table>
+  )
+}
+
+/**
+ * @returns {JSX.Element} The data table consisting of the data
+ */
+export default function DataTable() {
+  const [users, setUsers] = useState<User[]>([])
+  const { isLoading } = trpc.user.getAllUsers.useQuery(undefined, {
+    onSuccess(data) {
+      setUsers(data)
+    },
+  })
+
+  const columns = useMemo<ColumnDef<User>[]>(
+    () => [
+      {
+        header: 'Users',
+        columns: [
+          {
+            accessorKey: 'id',
+            cell: (info) => info.getValue(),
+            header: 'No.',
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorKey: 'name',
+            cell: (info) => info.getValue(),
+            header: 'Name',
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorKey: 'email',
+            cell: (info) => info.getValue(),
+            header: 'Email',
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorKey: 'department',
+            cell: (info) => info.getValue(),
+            header: 'Department',
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorKey: 'roles',
+            cell: (info) => info.getValue(),
+            header: 'Role',
+            footer: (props) => props.column.id,
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  if (isLoading) {
+    return <LoadingScreen />
+  }
+
+  return <Table data={users} columns={columns} />
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,24 +1,9 @@
-import Buttons from '~/components/admin/Buttons'
 import Layout from '~/components/common/Layout'
 import { useSession } from 'next-auth/react'
 import LoadingScreen from '~/components/common/LoadingScreen'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-import { trpc } from '~/utils/trpc'
-import { type User, TableBody, TableHeader } from '~/components/users/Table'
-import {
-  // type Column,
-  // type Table as ReactTable,
-  // PaginationState,
-  useReactTable,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  type ColumnDef,
-  // OnChangeFn,
-  // flexRender,
-} from '@tanstack/react-table'
-import { useMemo, useState } from 'react'
+import DataTable from '~/components/users/Table'
 
 const SEO = () => {
   return (
@@ -27,93 +12,6 @@ const SEO = () => {
       <link rel="icon" href="/favicon.ico" />
       <meta name="description" content="The login page for Atlas" />
     </Head>
-  )
-}
-
-const DataTable = () => {
-  const [users, setUsers] = useState<User[]>([])
-
-  const { isLoading } = trpc.user.getAllUsers.useQuery(undefined, {
-    onSuccess(data) {
-      setUsers(data)
-    },
-  })
-
-  // Return The React Table If The Table Is Rendered
-  const columns = useMemo<ColumnDef<User>[]>(
-    () => [
-      {
-        header: 'Users',
-        columns: [
-          {
-            accessorKey: 'id',
-            cell: (info) => info.getValue(),
-            header: 'No.',
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorKey: 'name',
-            cell: (info) => info.getValue(),
-            header: 'Name',
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorKey: 'email',
-            cell: (info) => info.getValue(),
-            header: 'Email',
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorKey: 'department',
-            cell: (info) => info.getValue(),
-            header: 'Department',
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorKey: 'roles',
-            cell: (info) => info.getValue(),
-            header: 'Role',
-            footer: (props) => props.column.id,
-          },
-        ],
-      },
-    ],
-    []
-  )
-
-  // Return Loading Screen If The Data Is Still Loading
-  if (isLoading) {
-    return <LoadingScreen />
-  }
-  return (
-    <>
-      <Table data={users} columns={columns} />
-    </>
-  )
-}
-
-const Table = ({
-  data,
-  columns,
-}: {
-  data: User[]
-  columns: ColumnDef<User>[]
-}) => {
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-  })
-
-  return (
-    <>
-      <table>
-        <TableHeader table={table} />
-        <TableBody table={table} />
-      </table>
-    </>
   )
 }
 
@@ -135,7 +33,6 @@ export default function AdminUserPage() {
       <Layout>
         <div className="flex flex-col">
           <DataTable />
-          <Buttons />
         </div>
       </Layout>
     </>
