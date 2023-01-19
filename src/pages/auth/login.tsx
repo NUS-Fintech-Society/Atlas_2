5229f8918ec854b6d131a1987618f2baf26b839c
woowenjun99 <woowenjun99@gmail.com>
2023-01-20 01:05:59 +0800

    Remove redundant code and refactor

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -26,11 +26,6 @@ const LoginPage = () => {
         redirect: false,
       })
 
-      if (res && res.ok) {
-        router.push('/users')
-        return
-      }
-
       if (res && res.error) {
         setSubmitting(false)
         toast({
@@ -52,10 +47,12 @@ const LoginPage = () => {
     }
   }
 
-  if (status === 'loading') return <LoadingScreen />
+  if (status === 'loading') {
+    return <LoadingScreen />
+  }
+
   if (session) {
-    if (session.level === 'admin') router.push('/admin')
-    router.push('/user')
+    router.push('/')
   }
 
   return (
