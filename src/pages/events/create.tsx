14fe791ee26c679a97817087a56d58c8fd424fc4
michaelseyo <michael.lcw99@gmail.com>
2023-01-23 15:49:39 +0800

    Update title of Next Head for create event

--- src/pages/events/create.tsx
+++ src/pages/events/create.tsx
@@ -15,6 +15,7 @@ import { trpc } from '../../utils/trpc'
 import { useSession } from 'next-auth/react'
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import Head from 'next/head'
 
 const EventPage = () => {
   const toast = useToast()
@@ -56,65 +57,72 @@ const EventPage = () => {
     return <div>Loading...</div>
   } else {
     return (
-      <div className="h-full items-center bg-black text-white">
-        <div className="mx-auto max-w-4xl p-10">
-          <h1 className="mb-10 text-center text-2xl font-bold">
-            Create New Event
-          </h1>
-          <form onSubmit={handleSubmit(formSubmit)}>
-            <VStack align="left" spacing="6">
-              <div>
-                <FormLabel>Event Name</FormLabel>
-                <Input {...register('eventName')} />
-              </div>
-              <div className="flex">
-                <FormLabel>Department</FormLabel>
-                <CheckboxGroup colorScheme="green">
-                  <Stack spacing={[1, 5]} direction={['row', 'column']}>
-                    <Checkbox value="ml" {...register('dept')}>
-                      Machine Learning
-                    </Checkbox>
-                    <Checkbox value="sd" {...register('dept')}>
-                      Software Development
-                    </Checkbox>
-                    <Checkbox value="bc" {...register('dept')}>
-                      Blockchain
-                    </Checkbox>
-                    <Checkbox value="ir" {...register('dept')}>
-                      Internal Relations
-                    </Checkbox>
-                    <Checkbox value="ea" {...register('dept')}>
-                      External Affairs
-                    </Checkbox>
-                  </Stack>
-                </CheckboxGroup>
-              </div>
-              <div>
-                <FormLabel>Date</FormLabel>
-                <Input
-                  placeholder="Select Date and Time"
-                  size="md"
-                  type="date"
-                  {...register('eventDate')}
-                />
-              </div>
-              <div className="flex items-center">
-                <FormLabel>QR Code required</FormLabel>
-                <Checkbox></Checkbox>
-              </div>
-              <DataTable data={data} setAttendees={setAttendees} />
-              <div className="flex justify-between">
-                <Button bgColor="#FF9900" width={150} textColor="black">
-                  Back
-                </Button>
-                <Button bgColor="#4365DD" width={150} type="submit">
-                  Create Event
-                </Button>
-              </div>
-            </VStack>
-          </form>
+      <>
+        <Head>
+          <title>Atlas | Create Event</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content="The create event page for Atlas" />
+        </Head>
+        <div className="h-full items-center bg-black text-white">
+          <div className="mx-auto max-w-4xl p-10">
+            <h1 className="mb-10 text-center text-2xl font-bold">
+              Create New Event
+            </h1>
+            <form onSubmit={handleSubmit(formSubmit)}>
+              <VStack align="left" spacing="6">
+                <div>
+                  <FormLabel>Event Name</FormLabel>
+                  <Input {...register('eventName')} />
+                </div>
+                <div className="flex">
+                  <FormLabel>Department</FormLabel>
+                  <CheckboxGroup colorScheme="green">
+                    <Stack spacing={[1, 5]} direction={['row', 'column']}>
+                      <Checkbox value="ml" {...register('dept')}>
+                        Machine Learning
+                      </Checkbox>
+                      <Checkbox value="sd" {...register('dept')}>
+                        Software Development
+                      </Checkbox>
+                      <Checkbox value="bc" {...register('dept')}>
+                        Blockchain
+                      </Checkbox>
+                      <Checkbox value="ir" {...register('dept')}>
+                        Internal Relations
+                      </Checkbox>
+                      <Checkbox value="ea" {...register('dept')}>
+                        External Affairs
+                      </Checkbox>
+                    </Stack>
+                  </CheckboxGroup>
+                </div>
+                <div>
+                  <FormLabel>Date</FormLabel>
+                  <Input
+                    placeholder="Select Date and Time"
+                    size="md"
+                    type="date"
+                    {...register('eventDate')}
+                  />
+                </div>
+                <div className="flex items-center">
+                  <FormLabel>QR Code required</FormLabel>
+                  <Checkbox></Checkbox>
+                </div>
+                <DataTable data={data} setAttendees={setAttendees} />
+                <div className="flex justify-between">
+                  <Button bgColor="#FF9900" width={150} textColor="black">
+                    Back
+                  </Button>
+                  <Button bgColor="#4365DD" width={150} type="submit">
+                    Create Event
+                  </Button>
+                </div>
+              </VStack>
+            </form>
+          </div>
         </div>
-      </div>
+      </>
     )
   }
 }
