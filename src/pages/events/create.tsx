cf41238a3ce34a6f27e7ae524ad90fc00ea4f444
michaelseyo <michael.lcw99@gmail.com>
2023-01-23 14:32:37 +0800

    Base form logic

--- src/pages/events/create.tsx
+++ src/pages/events/create.tsx
@@ -7,32 +7,46 @@ import {
   Stack,
   VStack,
   Button,
+  useToast,
+  FormErrorMessage,
 } from '@chakra-ui/react'
-import { DataTable, columns } from '../../components/events/DataTable'
+import { DataTable } from '../../components/events/DataTable'
 import { trpc } from '../../utils/trpc'
 import { useSession } from 'next-auth/react'
 import React, { useState } from 'react'
-//TODO: Make the form submit stuff
-//https://chakra-ui.com/docs/components/form-control
-//https://chakra-ui.com/docs/components/checkbox
+import { useForm } from 'react-hook-form'
 
 const EventPage = () => {
+  const toast = useToast()
+  // useForm for state management except attendees which belongs in DataTable (child)
+  const { register, handleSubmit } = useForm()
+  const [attendees, setAttendees] = useState<string[]>([])
+
   const { data: data } = trpc.event.getAllUsers.useQuery()
   const newEvent = trpc.event.createEvent.useMutation()
   const { data: session, status } = useSession()
-  const [eventName, setEventName] = useState('')
-  const [eventDate, setEventDate] = useState('')
-  const [eventDepart, setEventDepart] = useState<string[]>([])
-  const [attendee, setAttendee] = useState<string[]>([])
 
-  const updateEventDepart = (event: any) => {
-    event = event.valueOf().toString()
-    setEventDepart([...eventDepart, event])
-  }
-
-  const sendDataToParent = (att: string[]) => {
-    // the callback. Use a better name
-    setAttendee(att)
+  const formSubmit = (data: any) => {
+    try {
+      console.log('Form args:', data)
+      console.log('Selected attendees: ', attendees)
+      // newEvent.mutate({
+      //   name: data.eventName,
+      //   date: new Date(data.eventDate),
+      //   departments: data.dept,
+      //   attendees: attendees,
+      // })
+      toast({
+        description: 'A new event has been successfully created!',
+      })
+    } catch (e) {
+      toast({
+        description: (e as Error).message,
+        duration: 3000,
+        status: 'error',
+        title: 'Oops, an error occured!',
+      })
+    }
   }
 
   if (!data) {
@@ -42,33 +56,36 @@ const EventPage = () => {
     return <div>Loading...</div>
   } else {
     return (
-      <div className="h-fill items-center bg-black text-white">
+      <div className="h-full items-center bg-black text-white">
         <div className="mx-auto max-w-4xl p-10">
           <h1 className="mb-10 text-center text-2xl font-bold">
             Create New Event
           </h1>
-          <FormControl>
+          <form onSubmit={handleSubmit(formSubmit)}>
             <VStack align="left" spacing="6">
               <div>
                 <FormLabel>Event Name</FormLabel>
-                <Input
-                  type="text"
-                  value={eventName}
-                  onChange={(event) => setEventName(event.target.value)}
-                />
+                <Input {...register('eventName')} />
               </div>
               <div className="flex">
                 <FormLabel>Department</FormLabel>
-                <CheckboxGroup
-                  colorScheme="green"
-                  onChange={(event) => updateEventDepart(event)}
-                >
+                <CheckboxGroup colorScheme="green">
                   <Stack spacing={[1, 5]} direction={['row', 'column']}>
-                    <Checkbox value="ml">Machine Learning</Checkbox>
-                    <Checkbox value="sd">Software Development</Checkbox>
-                    <Checkbox value="bc">Blockchain</Checkbox>
-                    <Checkbox value="ir">Internal Relations</Checkbox>
-                    <Checkbox value="ea">External Affairs</Checkbox>
+                    <Checkbox value="ml" {...register('dept')}>
+                      Machine Learning
+                    </Checkbox>
+                    <Checkbox value="sd" {...register('dept')}>
+                      Software Development
+                    </Checkbox>
+                    <Checkbox value="bc" {...register('dept')}>
+                      Blockchain
+                    </Checkbox>
+                    <Checkbox value="ir" {...register('dept')}>
+                      Internal Relations
+                    </Checkbox>
+                    <Checkbox value="ea" {...register('dept')}>
+                      External Affairs
+                    </Checkbox>
                   </Stack>
                 </CheckboxGroup>
               </div>
@@ -78,48 +95,24 @@ const EventPage = () => {
                   placeholder="Select Date and Time"
                   size="md"
                   type="date"
-                  value={eventDate}
-                  onChange={(event) => setEventDate(event.target.value)}
+                  {...register('eventDate')}
                 />
               </div>
               <div className="flex items-center">
                 <FormLabel>QR Code required</FormLabel>
                 <Checkbox></Checkbox>
               </div>
-              <div>
-                <DataTable
-                  columns={columns}
-                  data={data}
-                  sendDataToParent={sendDataToParent}
-                />
-              </div>
+              <DataTable data={data} setAttendees={setAttendees} />
               <div className="flex justify-between">
                 <Button bgColor="#FF9900" width={150} textColor="black">
                   Back
                 </Button>
-                <Button
-                  bgColor="#4365DD"
-                  width={150}
-                  onClick={(event) => {
-                    event.preventDefault()
-
-                    newEvent.mutate({
-                      name: eventName,
-                      date: new Date(eventDate),
-                      departments: eventDepart,
-                      attendees: attendee,
-                    })
-                    setEventName('')
-                    setEventDate('')
-                    setEventDepart([])
-                    alert('A new event is successfully created!')
-                  }}
-                >
+                <Button bgColor="#4365DD" width={150} type="submit">
                   Create Event
                 </Button>
               </div>
             </VStack>
-          </FormControl>
+          </form>
         </div>
       </div>
     )
