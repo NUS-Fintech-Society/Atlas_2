a43057b3b53943b6bf6ae7a3847d7fe86482ba53
Nixon <nixonwidjaja23@gmail.com>
2023-01-27 23:58:04 +0800

    create dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,209 @@
-import { useSession } from 'next-auth/react'
-import LoadingScreen from '~/components/common/LoadingScreen'
-import Head from 'next/head'
+import React from 'react'
+import { trpc } from '../utils/trpc'
+import {
+  ExcoMembersInfo,
+  TopThreeInfo,
+  MembersInfo,
+} from './memberDashboard/constants'
 
-const HomePage = () => {
-  const { status } = useSession({ required: true })
+import {
+  Container,
+  Input,
+  Heading,
+  Text,
+  Grid,
+  GridItem,
+  Avatar,
+  Box,
+} from '@chakra-ui/react'
 
+// const{data: roles} = trpc.useQuery(["memberdash.getRoles"]) -> this will give you an array of object, each object has only 1
+// attribute called roles
+//     console.log(roles)
+//     {roles?.map((role,index) => {
+//         const{data: memberinfo} = trpc.useQuery(["memberdash.getAll",{roles: role.roles}]) -> this is to use each role from the prev query
+// to call the second query -> get all members info for this role, the output is also an array of object, each object has the person's info
+//         console.log(memberinfo)
+//     })}
+const Member = () => {
   return (
-    <>
-      <Head>
-        <title>Atlas</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="The home page for Atlas" />
-      </Head>
-      {status === 'loading' ? <LoadingScreen /> : <div>Home Page</div>}
-    </>
+    <Container centerContent padding="20px">
+      {/* Link : https://bit.ly/dan-abramov */}
+      <Avatar height="100px" width="100px" src="link" />
+      <Container centerContent paddingTop="20px">
+        <Text fontSize="20px" color="#FFFFFF">
+          {' '}
+          Name{' '}
+        </Text>
+        <Text fontSize="20px" color="#FFFFFF">
+          {' '}
+          Batch{' '}
+        </Text>
+      </Container>
+    </Container>
   )
 }
 
-export default HomePage
+const ExcoBoard = () => {
+  return (
+    <div>
+      <Container centerContent paddingTop="20px">
+        <Text fontSize="40px" color="#FF9900">
+          EXCO
+        </Text>
+      </Container>
+
+      <Grid
+        height="600px"
+        templateRows="repeat(2,1fr)"
+        templateColumns="repeat(7,1fr)"
+        gap="9"
+        paddingTop="30px"
+        paddingStart="150px"
+        paddingEnd="150px"
+      >
+        {TopThreeInfo.map((p, i) => {
+          return (
+            <GridItem colStart={2 * i + 2} colEnd={2 * i + 3} key={i}>
+              <Container centerContent padding="20px">
+                <Avatar height="100px" width="100px" src={p.image} />
+                <Container centerContent paddingTop="20px">
+                  <Text fontSize="25px" color="#FFFFFF">
+                    {' '}
+                    {p.role}{' '}
+                  </Text>
+                  <Text fontSize="20px" color="#FFFFFF">
+                    {' '}
+                    {p.name}
+                  </Text>
+                  <Text fontSize="20px" color="#FFFFFF">
+                    {' '}
+                    {p.batch}
+                  </Text>
+                </Container>
+              </Container>
+            </GridItem>
+          )
+        })}
+
+        {ExcoMembersInfo.map((p, i) => {
+          return (
+            <GridItem rowStart={2} rowSpan={1} colSpan={1} key={i}>
+              <Container centerContent padding="20px">
+                <Avatar height="100px" width="100px" src={p.image} />
+                <Container centerContent paddingTop="20px">
+                  <Text fontSize="25px" color="#FFFFFF">
+                    {' '}
+                    {p.role}{' '}
+                  </Text>
+                  <Text fontSize="20px" color="#FFFFFF">
+                    {' '}
+                    {p.name}
+                  </Text>
+                  <Text fontSize="20px" color="#FFFFFF">
+                    {' '}
+                    {p.batch}
+                  </Text>
+                </Container>
+              </Container>
+            </GridItem>
+          )
+        })}
+      </Grid>
+    </div>
+  )
+}
+
+const MemberBoard = () => {
+  const { data: roles } = trpc.member.getRoles.useQuery()
+  return (
+    <div>
+      <Container centerContent paddingTop="20px" bg="black">
+        <Text fontSize="40px" color="#FF9900">
+          Software Development
+        </Text>
+      </Container>
+      <Grid
+        height="600px"
+        templateColumns="repeat(7,1fr)"
+        gap="9"
+        paddingTop="30px"
+        paddingStart="150px"
+        paddingEnd="150px"
+      >
+        {roles?.map((p, i) => {
+          return (
+            <GridItem colSpan={2} rowStart={i + 1} key={i}>
+              <Container centerContent paddingTop="50px">
+                <Text fontSize="30px" color="#FFFFFF">
+                  {p.roles}
+                </Text>
+              </Container>
+            </GridItem>
+          )
+        })}
+
+        {roles?.map((p, i) => {
+          return (
+            <GridItem colSpan={5} rowStart={i + 1} key={i}>
+              {/* <Members roles = {p.roles}/> */}
+              {/* <Grid templateColumns='repeat(5,1fr)' gap='9'>
+                {p.members.map((m, j) => {
+                    return (
+                        <GridItem colSpan={1} key={j}>
+                          <Container centerContent padding='20px'>
+                              <Avatar height='100px' width='100px'  src={m.image}/>
+                              <Container centerContent paddingTop='20px'>
+                              <Text fontSize='20px' color='#FFFFFF'>{m.name}</Text>
+                              <Text fontSize='20px' color='#FFFFFF'>{m.batch}</Text>
+                              </Container>
+                      </Container>
+                        </GridItem>
+                    );
+                  })}
+                  </Grid> */}
+            </GridItem>
+          )
+        })}
+      </Grid>
+    </div>
+  )
+}
+
+export default function dashboard() {
+  return (
+    <div>
+      {/* <div className = "h-48 grid grid-cols-7 grid-rows-1 gap-4">
+            <div className = " col-span-2 flex justify-center items-center">
+                <p className = "text-3xl">Co-Directors</p></div>
+            <div className = "flex flex-col justify-center items-center">
+            <Avatar name='Dan Abrahmov' size='xl' src='https://bit.ly/dan-abramov' />
+            <p className= 'text-xl'>name</p>
+            <p>batch</p>
+            </div>
+        </div> */}
+      <Grid templateColumns="repeat(6,1fr)">
+        <GridItem colStart={3} colEnd={5}>
+          <Container centerContent>
+            <Heading fontSize="56px"> Our Members </Heading>
+          </Container>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Container centerContent paddingTop="20px">
+            <Input
+              borderRadius="10px"
+              placeholder="Search"
+              borderColor="#FF9900"
+              size="sm"
+              htmlSize={35}
+              width="auto"
+            />
+          </Container>
+        </GridItem>
+      </Grid>
+
+      <ExcoBoard />
+      <MemberBoard />
+    </div>
+  )
+}
---
+++ src/pages/memberDashboard/constants.tsx
@@ -0,0 +1,270 @@
+export const TopThreeInfo = [
+  {
+    name: '1',
+    image: '/images/1.png',
+    role: 'Role',
+    batch: '2021',
+    id: 0,
+  },
+  {
+    name: '2',
+    image: 'https://bit.ly/dan-abramov',
+    role: 'Role',
+    batch: '2021',
+    id: 1,
+  },
+  {
+    name: '3',
+    image: '/images/1.png',
+    role: 'Role',
+    batch: '2021',
+    id: 2,
+  },
+]
+
+export const ExcoMembersInfo = [
+  {
+    name: '1',
+    image: '/images/1.png',
+    role: 'Role',
+    batch: '2021',
+    id: 0,
+  },
+  {
+    name: '2',
+    image: 'https://bit.ly/dan-abramov',
+    role: 'Role',
+    batch: '2021',
+    id: 1,
+  },
+  {
+    name: '3',
+    image: '/images/1.png',
+    role: 'Role',
+    batch: '2021',
+    id: 2,
+  },
+  {
+    name: '4',
+    image: '/images/1.png',
+    role: 'Role',
+    batch: '2021',
+    id: 3,
+  },
+  {
+    name: '5',
+    image: '/images/1.png',
+    role: 'Role',
+    batch: '2021',
+    id: 4,
+  },
+  {
+    name: '6',
+    image: '/images/1.png',
+    role: 'Role',
+    batch: '2021',
+    id: 5,
+  },
+  {
+    name: '7',
+    image: '/images/1.png',
+    role: 'Role',
+    batch: '2021',
+    id: 6,
+  },
+]
+
+export const MembersInfo = [
+  {
+    role: 'Co-Directors',
+    members: [
+      {
+        name: '0',
+        image: 'https://bit.ly/dan-abramov',
+        batch: '2021',
+        id: 0,
+      },
+      {
+        name: '1',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 1,
+      },
+    ],
+    id: 0,
+  },
+  {
+    role: 'Head of Design',
+    members: [
+      {
+        name: '0',
+        image: 'https://bit.ly/dan-abramov',
+        batch: '2021',
+        id: 0,
+      },
+    ],
+    id: 1,
+  },
+  {
+    role: 'Design Managers',
+    members: [
+      {
+        name: '0',
+        image: 'https://bit.ly/dan-abramov',
+        batch: '2021',
+        id: 0,
+      },
+      {
+        name: '1',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 1,
+      },
+    ],
+    id: 2,
+  },
+  {
+    role: 'UI/UX Designers',
+    members: [
+      {
+        name: '0',
+        image: 'https://bit.ly/dan-abramov',
+        batch: '2021',
+        id: 0,
+      },
+      {
+        name: '1',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 1,
+      },
+      {
+        name: '2',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 2,
+      },
+      {
+        name: '3',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 3,
+      },
+      {
+        name: '4',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 4,
+      },
+    ],
+    id: 3,
+  },
+  {
+    role: 'Tech Leads',
+    members: [
+      {
+        name: '0',
+        image: 'https://bit.ly/dan-abramov',
+        batch: '2021',
+        id: 0,
+      },
+      {
+        name: '1',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 1,
+      },
+      {
+        name: '2',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 2,
+      },
+      {
+        name: '3',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 3,
+      },
+      {
+        name: '4',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 4,
+      },
+    ],
+    id: 4,
+  },
+  {
+    role: 'Software Engineers',
+    members: [
+      {
+        name: '0',
+        image: 'https://bit.ly/dan-abramov',
+        batch: '2021',
+        id: 0,
+      },
+      {
+        name: '1',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 1,
+      },
+      {
+        name: '2',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 2,
+      },
+      {
+        name: '3',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 3,
+      },
+      {
+        name: '4',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 4,
+      },
+    ],
+    id: 5,
+  },
+  {
+    role: 'Product Managers',
+    members: [
+      {
+        name: '0',
+        image: 'https://bit.ly/dan-abramov',
+        batch: '2021',
+        id: 0,
+      },
+      {
+        name: '1',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 1,
+      },
+      {
+        name: '2',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 2,
+      },
+      {
+        name: '3',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 3,
+      },
+      {
+        name: '4',
+        image: '/images/1.png',
+        batch: '2021',
+        id: 4,
+      },
+    ],
+    id: 6,
+  },
+]
