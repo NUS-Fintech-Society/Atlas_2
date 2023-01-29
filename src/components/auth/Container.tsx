a6ff6a429cfdf99439737df18b960b9abfb582e3
woowenjun99 <woowenjun99@gmail.com>
2023-01-29 23:23:31 +0800

    Remove the black background in layout and change button text to white

--- src/components/auth/Container.tsx
+++ src/components/auth/Container.tsx
@@ -1,6 +1,6 @@
 const Container = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
   return (
-    <div className="flex h-max w-screen items-center justify-center bg-black text-white">
+    <div className="flex h-max w-screen items-center justify-center">
       <div className="flex min-h-screen w-full flex-col items-center justify-center p-4">
         <div className="w-full max-w-3xl rounded-2xl p-8 shadow-lg transition-all duration-1000 hover:shadow-2xl">
           {children}
