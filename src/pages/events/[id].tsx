7bd8283bbf225e9cf16c7a90c7cf9f78244b7ccd
woowenjun99 <woowenjun99@gmail.com>
2023-02-18 11:04:08 +0800

    Shift the asynchronous code into try catch

--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -6,7 +6,6 @@ import LoadingScreen from '~/components/common/LoadingScreen'
 import Button from '~/components/utilities/Button'
 import Link from 'next/link'
 import Head from 'next/head'
-import dayjs from 'dayjs'
 import { type MouseEvent } from 'react'
 
 const NoImagePage = () => {
@@ -15,7 +14,7 @@ const NoImagePage = () => {
       <Head>
         <title>Atlas | No Event Found</title>
       </Head>
-      <body>
+      <div>
         <div className="w-full">
           {/* Warning Symbol */}
           <Image
@@ -27,11 +26,13 @@ const NoImagePage = () => {
           />
 
           <h1 className="text-center text-4xl font-bold">No Event Found!</h1>
-          <Link href="/">
-            <div className="bg-green-50">Return To Home Page</div>
-          </Link>
+          <div className="mt-5 flex justify-center">
+            <Link href="/">
+              <Button>Return Home</Button>
+            </Link>
+          </div>
         </div>
-      </body>
+      </div>
     </>
   )
 }
@@ -39,22 +40,16 @@ const NoImagePage = () => {
 const AdminAttendancePage = ({
   name,
   date,
-  departments,
   hasStarted,
   id,
   qrcode,
 }: {
-  date: Date
+  date: string
   name: string
-  departments: string[]
   hasStarted: boolean
   id: string
   qrcode: string
 }) => {
-  const start = dayjs(date).format('D MMMM YYYY')
-  const involvedDepartments = departments.map((department, index) => {
-    return <p key={index}>{department}</p>
-  })
   const { mutateAsync, isLoading } = trpc.event.startEvent.useMutation()
   async function startEvent(
     e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
@@ -68,26 +63,24 @@ const AdminAttendancePage = ({
       <Head>
         <title>Atlas | {name}</title>
       </Head>
-      <body className="min-h-screen bg-[#F5F5F5]">
+      <div className="min-h-screen bg-[#F5F5F5]">
         <div className="relative z-50">
           <h1 className="text-center font-[Inter] text-5xl font-bold">
             {name}
           </h1>
           <h2 className="my-14 text-center font-[Inter] text-3xl font-medium">
-            {start}
+            {date}
           </h2>
-          <div className="flexjustify-center">
+          <div className="flex justify-center">
             <Image alt="qrcode" height={300} src={qrcode} width={300} />
           </div>
-          {hasStarted ? (
-            involvedDepartments
-          ) : (
+          {hasStarted && (
             <Button onClick={startEvent} isLoading={isLoading} type="button">
               Start Event
             </Button>
           )}
         </div>
-      </body>
+      </div>
     </>
   )
 }
@@ -96,8 +89,9 @@ const EventPage = () => {
   const router = useRouter()
   useSession({ required: true })
   const slug = router.query.id
-  const { data, isLoading } = trpc.event.getEvent.useQuery(
-    typeof slug === 'string' ? slug : slug?.join()
+  const { data, isLoading, isError } = trpc.event.getEvent.useQuery(
+    typeof slug === 'string' ? slug : slug?.join(),
+    { enabled: false }
   )
   const render = () => {
     if (isLoading) {
@@ -110,17 +104,16 @@ const EventPage = () => {
         </>
       )
     }
-    if (!data?.qrcode) {
+    if (isError || !data) {
       return <NoImagePage />
     }
     return (
       <AdminAttendancePage
-        date={data.start as Date}
-        departments={data.departments as string[]}
-        id={slug}
+        date={data.start}
+        id={data.id as string}
         hasStarted={data.hasStarted as boolean}
         name={data.name as string}
-        qrcode={data.qrcode}
+        qrcode={data.qrcode as string}
       />
     )
   }
--- src/server/trpc/router/event/index.ts
+++ src/server/trpc/router/event/index.ts
@@ -29,48 +29,54 @@ const startEvent = protectedProcedure
 const getEvent = protectedProcedure
   .input(z.optional(z.string()))
   .query(async ({ ctx, input }) => {
-    const [event, qrcode] = await Promise.all([
-      ctx.prisma.event.findUnique({
-        where: { id: input },
-        select: {
-          departments: {
-            select: {
-              name: true,
+    try {
+      // Generate QR and fetch information about the event
+      const [event, qrcode] = await Promise.all([
+        ctx.prisma.event.findUnique({
+          where: { id: input },
+          select: {
+            departments: {
+              select: {
+                name: true,
+              },
             },
+            endDate: true,
+            hasStarted: true,
+            startDate: true,
+            name: true,
           },
-          endDate: true,
-          hasStarted: true,
-          startDate: true,
-          name: true,
-        },
-      }),
-      toDataURL(`${env.DOMAIN}/events/${input}`),
-    ])
+        }),
+        toDataURL(`${env.DOMAIN}/events/${input}`),
+      ])
 
-    // If there is no event, we return an empty object
-    if (!event) {
-      throw new TRPCError({
-        code: 'NOT_FOUND',
-        message: 'No such event exist',
-      })
-    }
+      // Throw an error if no such event exists.
+      if (!event) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'No such event exist',
+        })
+      }
 
-    // Check if the event has ended
-    const end = dayjs(event.endDate)
-    const now = dayjs()
-    if (now.isAfter(end)) {
-      throw new TRPCError({
-        code: 'BAD_REQUEST',
-        message: 'The event has already ended',
-      })
-    }
+      // Throw an error if the event has ended.
+      const end = dayjs(event.endDate)
+      const now = dayjs()
+      if (now.isAfter(end)) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'The event has already ended',
+        })
+      }
 
-    return {
-      departments: event.departments,
-      hasStarted: event.hasStarted,
-      name: event.name,
-      qrcode,
-      start: event.startDate,
+      return {
+        departments: event.departments,
+        hasStarted: event.hasStarted,
+        id: input,
+        name: event.name,
+        qrcode,
+        start: dayjs(event.startDate).format('D MMMM YYYY'),
+      }
+    } catch (e) {
+      console.error((e as Error).message)
     }
   })
 
