ce81072b611f544668815b102d9d2cd8feb8031b
woowenjun99 <woowenjun99@gmail.com>
2023-02-18 18:09:46 +0800

    Add hamburger navbar into home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useSession } from 'next-auth/react'
 import LoadingScreen from '~/components/common/LoadingScreen'
 import Head from 'next/head'
+import HamburgerNavbar from '~/components/common/HamburgerNavbar'
 
 const HomePage = () => {
   const { status } = useSession({ required: true })
@@ -12,6 +13,7 @@ const HomePage = () => {
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="The home page for Atlas" />
       </Head>
+      <HamburgerNavbar />
       {status === 'loading' ? <LoadingScreen /> : <div>Home Page</div>}
     </>
   )
