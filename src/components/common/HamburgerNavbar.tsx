6a997208cd51f1b17a4d5810a8450f393496f45d
woowenjun99 <woowenjun99@gmail.com>
2023-02-18 18:08:50 +0800

    Fix logout bug

--- src/components/common/HamburgerNavbar.tsx
+++ src/components/common/HamburgerNavbar.tsx
@@ -2,6 +2,7 @@ import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { signOut } from 'next-auth/react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 const navigation = [
   { name: 'Home', href: '/', current: true },
@@ -11,6 +12,7 @@ const navigation = [
 ]
 
 export default function HamburgerNavbar() {
+  const router = useRouter()
   return (
     <Disclosure as="nav" className="mb-3 bg-[#01003D]">
       {({ open }) => (
@@ -25,7 +27,7 @@ export default function HamburgerNavbar() {
               />
               <div className="absolute inset-y-0 right-0 flex items-center">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="text-white hover:bg-white hover:text-white focus:ring-white inline-flex items-center justify-center rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-inset">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon
@@ -56,7 +58,14 @@ export default function HamburgerNavbar() {
                   className="block px-3 py-2 hover:underline"
                   aria-current={item.current ? 'page' : undefined}
                   onClick={
-                    item.name === 'Sign Out' ? () => signOut() : undefined
+                    item.name === 'Sign Out'
+                      ? async () => {
+                          await signOut({
+                            redirect: false,
+                          })
+                          router.push('/auth/login')
+                        }
+                      : undefined
                   }
                 >
                   {item.name}
