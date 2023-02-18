d48c0ea386c67704404b32264f9f9f9011c50908
woowenjun99 <woowenjun99@gmail.com>
2023-02-18 15:25:43 +0800

    Refactor the code and wrote documentation

--- src/server/trpc/router/member/create.ts
+++ src/server/trpc/router/member/create.ts
@@ -3,11 +3,16 @@ import { z } from 'zod'
 import { TRPCError } from '@trpc/server'
 import { randomBytes } from 'crypto'
 import { hash } from 'bcryptjs'
-import dayjs from 'dayjs'
-import nodemailer from 'nodemailer'
-import { env } from '~/env/server.mjs'
 import type { User } from '@prisma/client'
-import type SMTPTransport from 'nodemailer/lib/smtp-transport'
+import {
+  checkUserPermission,
+  checkIfUserExist,
+  createNewUser,
+  sendEmail,
+  sendMultipleEmails,
+  buildUserObject,
+  createManyUsers,
+} from './helper'
 
 /**
  * Create multiple accounts for many users and send out an
@@ -39,93 +44,15 @@ export const addMultipleUsers = protectedProcedure
       })
     )
   )
-  .mutation(async ({ ctx, input }) => {
-    try {
-      const password = randomBytes(8).toString('hex')
-      const hashedPassword = await hash(password, 10)
+  .mutation(async ({ input }) => {
+    const password = randomBytes(10).toString('hex')
+    const hashedPassword = await hash(password, 10)
 
-      const users: User[] = input.map((user) => {
-        return {
-          attendance: 0,
-          batch: 'AY22/23',
-          department: user.department,
-          date_of_birth: dayjs().toDate(),
-          diet: user.diet,
-          discord: user.discord,
-          faculty: user.faculty,
-          gender: user.gender,
-          hashedPassword,
-          hobbies: user.hobbies,
-          image: null,
-          level: 'member',
-          linkedin: user.linkedin,
-          major: user.major,
-          id: user.student_id,
-          isAdmin: false,
-          name: user.name,
-          email: user.nus_email,
-          personal_email: user.personal_email,
-          phone: user.phone,
-          race: user.race,
-          roles: user.roles,
-          shirt: user.shirt,
-          telegram: user.telegram,
-          total_events: 0,
-          wallet: null,
-          year: user.year,
-        }
-      })
-
-      const promises: Promise<SMTPTransport.SentMessageInfo>[] = []
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: env.GMAIL,
-          pass: env.GMAIL_PASSWORD,
-        },
-      })
-
-      users.forEach(async (user) => {
-        const promise = transporter.sendMail({
-          from: env.GMAIL,
-          to: user.email,
-          subject: 'New Account Creation',
-          html: `
-            Hi User,
-            <br />
-            <p> We welcome you to Fintech Society. We hope you enjoy your
-            time here. In order to get you onboard, please login with 
-            the following password and change it immediately. </p>
-            <br />
-            <a href="${env.NEXTAUTH_URL}">HRMS Website</a>
-            <br />
-            The password is <strong>${password}</strong>
-            <br />
-            Thank You. <br /> 
-            Fintech HR
-            `,
-        })
-        promises.push(promise)
-      })
-
-      // Fulfil all of the promises concurrently
-      await Promise.all([
-        promises,
-        ctx.prisma.user.createMany({
-          data: users,
-        }),
-      ])
-    } catch (e) {
-      throw new TRPCError({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: (e as Error).message,
-      })
-    }
+    const users: User[] = buildUserObject(input, hashedPassword)
+    await createManyUsers(users)
+    await sendMultipleEmails(users, password)
   })
 
-/**
- * Creates a single user.
- */
 export const createSingleUser = protectedProcedure
   .input(
     z.object({
@@ -137,77 +64,11 @@ export const createSingleUser = protectedProcedure
     })
   )
   .mutation(async ({ ctx, input }) => {
-    try {
-      const personMakingRequest = await ctx.prisma.user.findUnique({
-        where: { id: ctx.session.user.id },
-      })
+    const { email, id, level, isAdmin } = input
+    const password = input.password || randomBytes(10).toString('hex')
 
-      // If the user is just a normal member, he should not be able to create a new account
-      if (!personMakingRequest || personMakingRequest.level === 'member') {
-        throw new TRPCError({
-          code: 'UNAUTHORIZED',
-          message: 'User is not authorized to create a new account',
-        })
-      }
-
-      const { email, id, level, isAdmin } = input
-
-      const password = input.password || randomBytes(10).toString('hex')
-
-      const hashedPassword = await hash(password, 10)
-
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: env.GMAIL,
-          pass: env.GMAIL_PASSWORD,
-        },
-      })
-
-      const foundUser = await ctx.prisma.user.findUnique({ where: { email } })
-      if (foundUser) {
-        throw new TRPCError({
-          code: 'CONFLICT',
-          message: 'User already exists',
-        })
-      }
-
-      await ctx.prisma.user.create({
-        data: {
-          batch: 'AY2022/2023',
-          name: '',
-          roles: '',
-          email,
-          id,
-          hashedPassword,
-          isAdmin,
-          level,
-        },
-      })
-
-      await transporter.sendMail({
-        from: env.GMAIL,
-        to: email,
-        subject: 'New Account Creation',
-        html: `
-      Hi User,
-      <br />
-      <p> We welcome you to Fintech Society. We hope you enjoy your
-      time here. In order to get you onboard, please login with 
-      the following password and change it immediately. </p>
-      <br />
-      <a href="${env.NEXTAUTH_URL}">HRMS Website</a>
-      <br />
-      The password is <strong>${password}</strong>
-      <br />
-      Thank You. <br /> 
-      Fintech HR
-      `,
-      })
-    } catch (e) {
-      throw new TRPCError({
-        code: 'BAD_REQUEST',
-        message: (e as Error).message,
-      })
-    }
+    await checkUserPermission(ctx.session.user.id)
+    await checkIfUserExist(email)
+    await createNewUser(email, id, isAdmin, level, password)
+    await sendEmail(email, password)
   })
