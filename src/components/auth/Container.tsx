798c610ff13d9039d92ad3819317cbcd132b95d0
michaelseyo <76872115+michaelseyo@users.noreply.github.com>
2023-02-26 23:36:31 +0800

    fix content overflowing navbar

--- src/components/auth/Container.tsx
+++ src/components/auth/Container.tsx
@@ -1,7 +1,7 @@
 const Container = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center text-black">
-      <div className="flex min-h-screen w-full flex-col items-center justify-center p-4">
+    <div className="relative h-screen w-screen items-center justify-center text-black">
+      <div className="flex min-h-screen w-full flex-col items-center justify-center p-2">
         <div className="w-full max-w-3xl rounded-2xl p-6 transition-all">
           {children}
         </div>
