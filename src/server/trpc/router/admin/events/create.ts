c565616587ff0aa15d23f70181cd7032da901dde
woowenjun99 <woowenjun99@gmail.com>
2023-03-07 09:12:42 +0800

    Add in the qrcode into the backend.

--- src/server/trpc/router/admin/events/create.ts
+++ src/server/trpc/router/admin/events/create.ts
@@ -1,6 +1,8 @@
 import { protectedProcedure } from '~/server/trpc/trpc'
 import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
+import { toDataURL } from 'qrcode'
+import { randomUUID } from 'crypto'
 
 export const createEvent = protectedProcedure
   .input(
@@ -10,21 +12,29 @@ export const createEvent = protectedProcedure
       endDate: z.date(),
       departments: z.array(z.string()),
       attendees: z.array(z.string()),
+      isQrRequired: z.boolean(),
     })
   )
-  .mutation(async ({ input }) => {
-    const prisma = new PrismaClient()
+  .mutation(async ({ ctx, input }) => {
+    let qr_code: string | undefined
+    const id = randomUUID()
 
-    await prisma.event.create({
+    if (input.isQrRequired) {
+      qr_code = await toDataURL(id)
+    }
+
+    await ctx.prisma.event.create({
       data: {
         attendees: {
           connect: input.attendees.map((attendee) => {
             return { id: attendee }
           }),
         },
+        id: input.isQrRequired ? id : randomUUID(),
         name: input.name,
         startDate: input.startDate,
         endDate: input.endDate,
+        qr_code,
       },
     })
   })
