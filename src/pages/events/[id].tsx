4a6eb788618db51dcabab3beee0f2324e498e296
woowenjun99 <woowenjun99@gmail.com>
2023-03-10 21:41:18 +0800

    Add in useSession hook

--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -1,7 +1,10 @@
 import { trpc } from '~/utils/trpc'
 import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
+import LoadingScreen from '~/components/common/LoadingScreen'
 
 const EventPage = () => {
+  const { data: session, status } = useSession({ required: true })
   const router = useRouter()
   const getId = () => {
     const { pid } = router.query
@@ -16,6 +19,11 @@ const EventPage = () => {
     return pid.join('')
   }
   const { data, error, isLoading } = trpc.event.getEvent.useQuery(getId())
+
+  if (status === 'loading') {
+    return <LoadingScreen />
+  }
+
   return <h1>Hiß</h1>
 }
 
