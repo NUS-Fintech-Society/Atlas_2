6b68fa06dbcfc8ed3130cabdaca0e3ec4de1ac4c
woowenjun99 <woowenjun99@gmail.com>
2023-03-12 16:56:53 +0800

    Remove the email and id to be updated

--- src/server/trpc/router/member/update.ts
+++ src/server/trpc/router/member/update.ts
@@ -20,28 +20,22 @@ export const updateMemberImage = protectedProcedure
     return user
   })
 
-  export const updateProfile = protectedProcedure
+export const updateProfile = protectedProcedure
   .input(
     z.object({
       id: z.string(),
       name: z.string(),
-      email: z.string(),
       roles: z.string(),
       department: z.string(),
     })
   )
   .mutation(async ({ ctx, input }) => {
-    console.log("Wen Jun", input);
-    const user = await ctx.prisma.user.update({
-      where: {
-        id: input.id.trim(),
-      },
+    await ctx.prisma.user.update({
+      where: { id: input.id },
       data: {
         department: input.department,
-        id: input.id,
         name: input.name,
         roles: input.roles,
       },
     })
-    return user
   })
