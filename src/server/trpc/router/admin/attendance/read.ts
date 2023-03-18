539f09e150cf54c500fea768f0861270d0047617
woowenjun99 <woowenjun99@gmail.com>
2023-03-18 09:12:02 +0800

    Rewrite the prisma syntax in NoSQL

--- src/server/trpc/router/admin/attendance/read.ts
+++ src/server/trpc/router/admin/attendance/read.ts
@@ -3,12 +3,26 @@ import { TRPCError } from '@trpc/server'
 
 export const getAllAttendance = protectedProcedure.query(async ({ ctx }) => {
   try {
-    const attendance = await ctx.prisma
-      .$queryRaw`SELECT e."name", COUNT(*) AS total_users, e."startDate", e."endDate", COALESCE((SELECT COUNT(*) FROM "_attendanceToUser" au JOIN "Attendance" a ON au."A"=a."id" AND a."eventId" = e."id" GROUP BY a."eventId"), 0) AS total_attendees
-      FROM "_EventToUser" eu
-      JOIN "Event" e ON eu."A" = e."id"
-      GROUP BY e."id";`
-    return attendance
+    const queries = await ctx.prisma.event.findMany({
+      select: {
+        name: true,
+        startDate: true,
+        endDate: true,
+        _count: {
+          select: { Attendance: true, attendees: true },
+        },
+      },
+    })
+
+    return queries.map((query) => {
+      return {
+        endDate: query.endDate,
+        name: query.name,
+        startDate: query.startDate,
+        total_attendees: query._count.Attendance,
+        total_users: query._count.attendees,
+      }
+    })
   } catch (e) {
     throw new TRPCError({
       code: 'INTERNAL_SERVER_ERROR',
