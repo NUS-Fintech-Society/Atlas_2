1549fc63271f590b5015f0380450be13beb64658
michaelseyo <76872115+michaelseyo@users.noreply.github.com>
2023-05-14 16:07:13 +0800

    base layout with responsive searchbar

---
+++ src/components/common/SearchBar.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
+
+const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState('')
+  return (
+    <InputGroup
+      minWidth={'150px'}
+      maxWidth={'300px'}
+      position="absolute"
+      top={{ lg: '0' }}
+      right={{ lg: '12', base: '0' }}
+      left={{ lg: 'unset', base: '0' }}
+      marginTop={{ lg: 'unset', base: '5' }}
+      marginLeft={{ base: 'auto' }}
+      marginRight={{ base: 'auto' }}
+    >
+      <InputLeftElement>
+        <SearchIcon />
+      </InputLeftElement>
+      <Input
+        placeholder="Search"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        borderWidth="3px"
+        border="3px solid #004586"
+        borderColor="#004586"
+        borderRadius="20px"
+      ></Input>
+    </InputGroup>
+  )
+}
+
+export default SearchBar
