7fa53f3278ba9e2392973a82f207ab787860af5c
michaelseyo <76872115+michaelseyo@users.noreply.github.com>
2023-05-22 22:43:07 +0800

    impl applicant endpoints for read

--- src/server/trpc/router/admin/recruitment/index.ts
+++ src/server/trpc/router/admin/recruitment/index.ts
@@ -1,7 +1,10 @@
 import { router } from '~/server/trpc/trpc'
 import { updateAppliedRoleStatus, updateInterviewNotes } from './update'
+import { getAllApplicants, getApplicant } from './read'
 
 export const recruitmentRouter = router({
   updateAppliedRoleStatus,
   updateInterviewNotes,
+  getAllApplicants,
+  getApplicant,
 })
