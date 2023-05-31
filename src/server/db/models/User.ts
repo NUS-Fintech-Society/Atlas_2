2fc80b7b113f1866ab68a2dddb3745ba9eba29f6
woowenjun99 <woowenjun99@gmail.com>
2023-05-31 21:59:24 +0800

    Update user model

--- src/server/db/models/User.ts
+++ src/server/db/models/User.ts
@@ -4,7 +4,18 @@ export type User = {
   hashedPassword: string
   isAdmin: boolean
   id?: string
-  image?: string
   name: string
   role: string
+
+  // Optional field: User can upload their image later.
+  image?: string
+
+  // Optional fields in create single user.
+  batch?: string
+  diet?: string
+  course?: string
+  linkedin?: string
+  phone?: string
+  telegram?: string
+  dob: string // Store in DD-MM format so that we can get their birthday easily.
 }
