24bc24276ef5879ace908ce84d0984af45be1e92
woowenjun99 <woowenjun99@gmail.com>
2023-06-01 21:08:31 +0800

    Make the interviewNotes optional

---
+++ src/server/db/models/AppliedRole.ts
@@ -0,0 +1,14 @@
+type Roles = {
+  rank: 1 | 2 | 3
+  department: string
+  role: string
+  status: 'accepted' | 'offered' | 'pending' | 'interviewed' | 'rejected'
+  interviewNotes?: string
+}[]
+
+export type AppliedRole = {
+  email: string
+  id?: string
+  name: string
+  roles: Roles
+}
