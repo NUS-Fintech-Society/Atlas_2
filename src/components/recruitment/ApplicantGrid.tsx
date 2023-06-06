bf5dd29da7ace9b8ba9fde4028a56faf4bf73b6d
HAO ZEYU <823549721@qq.com>
2023-06-06 23:51:08 +0800

    add filter button

--- src/components/recruitment/ApplicantGrid.tsx
+++ src/components/recruitment/ApplicantGrid.tsx
@@ -1,11 +1,12 @@
 import { useContext } from 'react'
-import { searchContext } from '~/context/recruitment/SearchProvider'
+import { filterContext, searchContext } from '~/context/recruitment/SearchProvider'
 import { trpc } from '~/utils/trpc'
 import ApplicantCard from './ApplicantCard'
 import InfoPopup from '~/components/recruitment/InfoPopup'
 
 const ApplicantGrid = () => {
   const { search } = useContext(searchContext)
+  const { filter } = useContext(filterContext)
   const { data } = trpc.recruitment.getAllApplicantsTopRoleByDept.useQuery()
   if (!data) return <></>
 
@@ -15,6 +16,8 @@ const ApplicantGrid = () => {
       : data?.filter((applicant) => {
           return applicant.name.toLowerCase().includes(search.toLowerCase())
         })
+
+  
 
   return (
     <>
