8a3d06a3d63b3b24397d4e334293b35df70e0de8
HAO ZEYU <823549721@qq.com>
2023-06-17 17:46:14 +0800

    add number of applicants for the recruitment page

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -3,34 +3,231 @@ import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import { SearchIcon } from '@chakra-ui/icons'
 import { searchContext } from '~/context/recruitment/SearchProvider'
 import { ChevronDownIcon } from '@chakra-ui/icons';
+import { useSession } from 'next-auth/react';
+
 
 const SearchBar = () => {
-  const { search, setSearch } = useContext(searchContext)
+  const roles = [
+    {
+      department: 'Software Development',
+      role: 'Software Engineer',
+    },
+    {
+      department: 'Software Development',
+      role: 'Technical Lead',
+    },
+    {
+      department: 'Software Development',
+      role: 'UI/UX Designer',
+    },
+    {
+      department: 'Software Development',
+      role: 'Head of Design',
+    },
+    {
+      department: 'Software Development',
+      role: 'Co-Director',
+    },
+    {
+      department: 'Blockchain',
+      role: 'Co-Director',
+    },
+    {
+      department: 'Blockchain',
+      role: 'Core Developer',
+    },
+    {
+      department: 'Blockchain',
+      role: 'Community Manager',
+    },
+    {
+      department: 'Blockchain',
+      role: 'Developer',
+    },
+    {
+      department: 'Blockchain',
+      role: 'Lead Developer',
+    },
+    {
+      department: 'Blockchain',
+      role: 'Research Analyst',
+    },
+    {
+      department: 'Blockchain',
+      role: 'Research Community Developer',
+    },
+    {
+      department: 'Machine Learning',
+      role: 'Quant Tech Analyst',
+    },
+    ,
+    {
+      department: 'Machine Learning',
+      role: 'Quant Tech Lead',
+    },
+    {
+      department: 'Machine Learning',
+      role: 'Quant wing head',
+    },
+    {
+      department: 'Machine Learning',
+      role: 'Project Tech Analyst',
+    },
+    {
+      department: 'Machine Learning',
+      role: 'Project Tech Lead',
+    },
+    {
+      department: 'Machine Learning',
+      role: 'Tech Analyst/Trainee',
+    },
+    {
+      department: 'Machine Learning',
+      role: 'Training Head',
+    },
+    {
+      department: 'Internal Affairs',
+      role: 'Co-Director',
+    },
+    {
+      department: 'Internal Affairs',
+      role: 'Community Development Executive',
+    },
+    {
+      department: 'Internal Affairs',
+      role: 'Community Development Lead',
+    },
+    {
+      department: 'Internal Affairs',
+      role: 'Finance Executive',
+    },
+    {
+      department: 'Internal Affairs',
+      role: 'Finance Lead',
+    },
+    {
+      department: 'Internal Affairs',
+      role: 'Project Manager Executive',
+    },
+    {
+      department: 'Internal Affairs',
+      role: 'Product Manager',
+    },
+    {
+      department: 'Internal Affairs',
+      role: 'Talent Management Executive',
+    },
+    {
+      department: 'Internal Affairs',
+      role: 'Talent Management Lead',
+    },
+    {
+      department: 'External Relations',
+      role: 'Partnerships Executive',
+    },
+    {
+      department: 'External Relations',
+      role: 'Director',
+    },
+    {
+      department: 'External Relations',
+      role: 'Marketing Executive',
+    },
+    {
+      department: 'External Relations',
+      role: 'Marketing Lead',
+    },
+    {
+      department: 'External Relations',
+      role: 'Partnerships Lead',
+    },
+    {
+      department: 'External Relations',
+      role: 'Finance Executive',
+    },
+    {
+      department: 'External Relations',
+      role: 'Finance Lead',
+    },
+  ] as { department: string; role: string }[]
+  const ctx = useSession()
+  const department = ctx.data?.user?.department
+  console.log(department)
+  const filteredRoles = roles.filter((combination) => combination.department === department)
+  const { search, filter, setFilter, setSearch } = useContext(searchContext)
   const [showDropdown, setShowDropdown] = useState(false);
-  const [selectedFilter, setSelectedFilter] = useState('');
+  console.log(filteredRoles)
+
+
+  const [selectedFilter, setSelectedFilter] = useState('')
   const [showOptions, setShowOptions] = useState(false);
   console.log('searchbar: ', search)
 
-  const handleFilterChange = (filter: React.SetStateAction<string>) => {
+  const handleFilterChange = (filter: string) => {
     setSelectedFilter(filter);
     setShowDropdown(false);
     // Perform filtering logic or update your data based on the selected filter
   };
 
+ 
+
   const renderOptions = () => {
     if (selectedFilter === 'Roles') {
       return (
         <ul className="ml-2">
-          <li className="px-3 py-2 cursor-pointer">Role 1</li>
-          <li className="px-3 py-2 cursor-pointer">Role 2</li>
+           {filteredRoles.map((role) =>   
+          <button 
+          key={role.role}
+          onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter(role.role)}}
+          className="px-3 py-2 cursor-pointer">{role.role}
+          </button>)
+          }
+
+          {/* <button 
+          onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('designer')}}
+          className="px-3 py-2 cursor-pointer">Designer</button>
+          <button 
+           onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('designer manager')}}
+          className="px-3 py-2 cursor-pointer">Designer Manager</button>
+          <button 
+           onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('technical lead')}}
+          className="px-3 py-2 cursor-pointer">Technical Lead</button>
+          <button 
+           onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('software engineer')}}
+          className="px-3 py-2 cursor-pointer">Software Engineer</button> */}
         </ul>
       );
     }
     if (selectedFilter === 'Status') {
       return (
         <ul className="ml-2">
-          <li className="px-3 py-2 cursor-pointer">Status 1</li>
-          <li className="px-3 py-2 cursor-pointer">Status 2</li>
+          <button 
+          onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('accepted')}}
+          className="px-3 py-2 cursor-pointer">Accepted</button>
+          <button 
+          onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('offered')}}
+          className="px-3 py-2 cursor-pointer">Offered</button>
+           <button 
+          onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('pending')}}
+          className="px-3 py-2 cursor-pointer">Pending Review</button>
+           <button 
+          onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('interviewed')}}
+          className="px-3 py-2 cursor-pointer">Interviewed</button>
+           <button 
+          onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('rejected')}}
+          className="px-3 py-2 cursor-pointer">Rejected</button>
+        </ul>
+      );
+    }
+    if (selectedFilter === 'Flags') {
+      return (
+        <ul className="ml-2">
+          <button 
+          onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('flagged')}}
+          className="px-3 py-2 cursor-pointer">Flagged</button>
+          <button 
+          onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('unflagged')}}
+          className="px-3 py-2 cursor-pointer">Unflagged</button>
         </ul>
       );
     }
@@ -43,7 +240,7 @@ const SearchBar = () => {
       maxWidth={'300px'}
       position="absolute"
       top={{ lg: '0' }}
-      right={{ lg: '12', base: '0' }}
+      right={{ lg: '20', base: '0' }}
       left={{ lg: 'unset', base: '0' }}
       marginTop={{ lg: 'unset', base: '5' }}
       marginLeft={{ base: 'auto' }}
@@ -73,9 +270,10 @@ const SearchBar = () => {
       >
         <ChevronDownIcon />
         {showOptions && (
-          <div className="absolute top-0 right-0 mt-10 bg-white border border-gray-200 rounded-md shadow-md">
+          <div className="absolute top-0 left-0 mt-10 bg-white border border-gray-200 rounded-md shadow-md">
             {showDropdown ? (
               <>
+               
                 <button
                   type="button"
                   className="px-4 py-2 cursor-pointer"
@@ -90,6 +288,20 @@ const SearchBar = () => {
                 >
                   Status
                 </button>
+                <button
+                  type="button"
+                  className="px-4 py-2 cursor-pointer"
+                  onClick={() => handleFilterChange('Flags')}
+                >
+                  Flags
+                </button>
+                <button
+                  type="button"
+                  className="px-4 py-2 mb-2 cursor-pointer rounded font-bold bg-gray-200"
+                  onClick={() => {setShowOptions(false), setShowDropdown(true), setFilter('')}}
+                >
+                  Clear
+                </button>
               </>
             ) : (
               renderOptions()
@@ -97,6 +309,7 @@ const SearchBar = () => {
           </div>
         )}
       </button>
+     
      
     </InputGroup>
   )
