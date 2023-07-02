b2b27afdae05dde591bf05a027386f428136a5f3
woowenjun99 <woowenjun99@gmail.com>
2023-07-02 17:49:52 +0800

    Standardise the login message

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -65,7 +65,7 @@ export const authOptions: NextAuthOptions = {
           const user = users[0] as User
           const isSuccess = await compare(password, user.hashedPassword)
           if (!isSuccess) {
-            throw Error('Incorrect password')
+            throw Error('Invalid email or password')
           }
 
           // The user object is passed to the session callback in session.data.user
