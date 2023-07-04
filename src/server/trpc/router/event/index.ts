ce0a814d6fddca0933d15d16309e37dbeec32eb5
woowenjun99 <woowenjun99@gmail.com>
2023-07-05 00:09:22 +0800

    Add in the populateCalendar method and integrate with main calendar

--- src/server/trpc/router/event/index.ts
+++ src/server/trpc/router/event/index.ts
@@ -4,8 +4,10 @@ import { updateEvent } from './updateEvent'
 import { deleteEvent } from './deleteEvent'
 import { createEvent } from './createEvent'
 import { getEventForUser } from './getEventForUser'
+import { populateCalendar } from './populateCalendar'
 
 export const eventRouter = router({
+  populateCalendar,
   getEvent,
   updateEvent,
   deleteEvent,
