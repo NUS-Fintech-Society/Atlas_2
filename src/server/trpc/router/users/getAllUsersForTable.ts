870d5f55ecd0c3b819e958ccac7cb0daa37d3f65
woowenjun99 <woowenjun99@gmail.com>
2023-07-04 23:24:13 +0800

    Remove pagination from the user table

--- src/server/trpc/router/users/getAllUsersForTable.ts
+++ src/server/trpc/router/users/getAllUsersForTable.ts
@@ -1,37 +1,24 @@
 import { protectedProcedure } from '~/server/trpc/trpc'
 import { TRPCError } from '@trpc/server'
-import { z } from 'zod'
 import userCollection from '~/server/db/collections/UserCollection'
 
-export const getAllUsersForTable = protectedProcedure
-  .input(
-    z.object({
-      pageIndex: z.number(),
-      pageSize: z.number(),
+export const getAllUsersForTable = protectedProcedure.query(async () => {
+  try {
+    const users = (await userCollection.queries([])).map((user) => {
+      return {
+        department: user.department,
+        email: user.email,
+        id: user.id as string,
+        name: user.name,
+        role: user.role,
+      }
     })
-  )
-  .query(async ({ input }) => {
-    // TODO: Fix up the pagination logic
-    try {
-      const users = (await userCollection.queries([])).map((user) => {
-        return {
-          department: user.department,
-          email: user.email,
-          id: user.id as string,
-          name: user.name,
-          role: user.role,
-          isAdmin: user.isAdmin,
-        }
-      })
 
-      const total = await userCollection.count()
-      const pageCount = Math.ceil(total.data().count / input.pageSize)
-
-      return { users, pageCount }
-    } catch (e) {
-      throw new TRPCError({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: (e as Error).message,
-      })
-    }
-  })
+    return users
+  } catch (e) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: (e as Error).message,
+    })
+  }
+})
