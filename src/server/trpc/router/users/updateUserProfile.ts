0c04eb89897bcc85c224e0e3633824263bd8d43c
woowenjun99 <woowenjun99@gmail.com>
2023-07-04 23:31:53 +0800

    Fix bug where the user profile is not updated in the modal

--- src/server/trpc/router/users/updateUserProfile.ts
+++ src/server/trpc/router/users/updateUserProfile.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from '@trpc/server'
 import { protectedProcedure } from '../../trpc'
 import { z } from 'zod'
 import userCollection from '~/server/db/collections/UserCollection'
@@ -11,10 +12,23 @@ export const updateUserProfile = protectedProcedure
       department: z.string(),
     })
   )
-  .mutation(async ({ ctx, input }) => {
-    await userCollection.update(ctx.session.user.id, {
-      department: input.department,
-      name: input.name,
-      role: input.role,
-    })
+  .mutation(async ({ input, ctx }) => {
+    try {
+      // Check whether the user has permissions.
+      const user = await userCollection.getById(ctx.session.user.id)
+      if (!user.isAdmin) {
+        throw new Error('User does not have permission to edit.')
+      }
+
+      await userCollection.update(input.id, {
+        department: input.department,
+        name: input.name,
+        role: input.role,
+      })
+    } catch (e) {
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: (e as Error).message,
+      })
+    }
   })
