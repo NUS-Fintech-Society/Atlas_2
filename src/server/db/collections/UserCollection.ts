25cf6ccb2f52d8401d61c700a39d04e52cd3f9c5
woowenjun99 <woowenjun99@gmail.com>
2023-07-11 20:47:39 +0800

    Tidy up the createTask

--- src/server/db/collections/UserCollection.ts
+++ src/server/db/collections/UserCollection.ts
@@ -7,18 +7,9 @@ class UserCollection extends BaseCollection<User> {
   protected override objectName = 'user'
 
   async findByDepartment(departments: string[]) {
-    const querySnapshot = await this.queries([
+    return await this.queries([
       where('department', 'in', departments),
     ])
-
-    const users = await Promise.all(
-      querySnapshot.map(async (snapshot) => {
-        const user = await this.getById(snapshot.id)
-        return user
-      })
-    )
-
-    return users
   }
 }
 
